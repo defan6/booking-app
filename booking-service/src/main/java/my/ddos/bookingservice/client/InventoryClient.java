30a2c4ff83038e74de5144c70db313756f929e8e
defan6 <valayevfedya2005@gmail.com>
2025-11-10 17:52:38 +0300

    flyway -> liquibase(advantages:rollback)

--- booking-service/src/main/java/my/ddos/bookingservice/client/InventoryClient.java
+++ booking-service/src/main/java/my/ddos/bookingservice/client/InventoryClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "inventory-service", url = "http://localhost:8081")
+@FeignClient(name = "inventory-service", url = "${inventory.service.url:http://localhost:7081}")
 public interface InventoryClient {
 
     @GetMapping("/api/v1/inventory/events/{eventId}")
